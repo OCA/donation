256ab5fcc43f6802b890079f1f32bd61f5f90d66
Alexis de Lattre <alexis@via.ecp.fr>
2014-06-13 23:41:02 +0200

    Rename field donation_campaign_id to campaign_id FIX invalidation of field 'Amount in Company Currency' on donation.line Add reporting menu for donations

--- donation/report/__init__.py
+++ donation/report/__init__.py
@@ -19,7 +19,4 @@
 #
 ##############################################################################
 
-from . import donation_campaign
-from . import donation
-from . import product
-from . import report
+from . import donation_report
