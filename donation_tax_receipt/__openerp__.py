fcf16cd5515155962e6765abefc396abf3dc4264
Alexis de Lattre <alexis@via.ecp.fr>
2015-01-31 23:09:57 +0100

    Add yaml test for annual tax receipts Move the content of donation/product_demo.xml inside donation/donation_demo.xml

--- donation_tax_receipt/__openerp__.py
+++ donation_tax_receipt/__openerp__.py
@@ -54,5 +54,8 @@ Barroux Abbey and by Alexis de Lattre from Akretion.
         'security/tax_receipt_security.xml',
         ],
     'demo': ['donation_tax_demo.xml'],
-    'test': ['test/each_tax_receipt.yml'],
+    'test': [
+        'test/each_tax_receipt.yml',
+        'test/annual_tax_receipt.yml',
+        ],
 }
