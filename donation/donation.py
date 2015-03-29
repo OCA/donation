c167162cb1fc9c4604ccb86026e66527ae06eaa7
Alexis de Lattre <alexis@via.ecp.fr>
2015-03-30 00:36:00 +0200

    Write a real README.md for the project Fix flake8 warning

--- donation/donation.py
+++ donation/donation.py
@@ -28,6 +28,7 @@ import openerp.addons.decimal_precision as dp
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    # begin with context_ to allow user to change it by himself
     context_donation_campaign_id = fields.Many2one(
         'donation.campaign', string='Current Donation Campaign')
     context_donation_journal_id = fields.Many2one(
@@ -36,7 +37,6 @@ class ResUsers(models.Model):
             ('type', 'in', ('bank', 'cash')),
             ('allow_donation', '=', True)],
         company_dependent=True)
-        # begin with context_ to allow user to change it by itself
 
 
 class DonationDonation(models.Model):
