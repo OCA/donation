467c5026936227b299f7e42d16645ee38d66058c
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-22 17:09:00 +0200

    Start the update to new architecture explained in issue #22
    Move from YAML tests to unittest in most modules
    Use commercial_partner_id for tax receipts (to group tax receipts for a company or a familly)
    On donation.line, tax_receipt_ok and in_kind are now related stored fields

---
+++ donation_base/wizard/tax_receipt_print.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Barroux Abbey (http://www.barroux.org)
+# © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, _
+from openerp.exceptions import UserError
+
+
+class DonationTaxReceiptPrint(models.TransientModel):
+    _name = 'donation.tax.receipt.print'
+    _description = 'Print Donation Tax Receipt'
+
+    @api.model
+    def _get_receipts(self):
+        return self.env['donation.tax.receipt'].search(
+            [('print_date', '=', False)])
+
+    receipt_ids = fields.Many2many(
+        'donation.tax.receipt',
+        column1='print_wizard_id', column2='receipt_id',
+        string='Receipts To Print', default=_get_receipts)
+
+    @api.multi
+    def print_receipts(self):
+        self.ensure_one()
+        if not self.receipt_ids:
+            raise UserError(
+                _('There are no tax receipts to print.'))
+        datas = {
+            'model': 'donation.tax.receipt',
+            'ids': self.receipt_ids.ids,
+        }
+        today = fields.Date.context_today(self)
+        self.receipt_ids.write({'print_date': today})
+        action = {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'donation_tax_receipt.report_donationtaxreceipt',
+            'data': datas,
+            'datas': datas,  # for Aeroo
+        }
+        return action
