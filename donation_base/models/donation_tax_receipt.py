c427f0f424b10ebc5bb990cab7b311219d4cf4be
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-22 17:09:00 +0200

    Start the update to new architecture explained in issue #22
    Move from YAML tests to unittest in most modules
    Use commercial_partner_id for tax receipts (to group tax receipts for a company or a familly)
    On donation.line, tax_receipt_ok and in_kind are now related stored fields

---
+++ donation_base/models/donation_tax_receipt.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Barroux Abbey (http://www.barroux.org)
+# © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
+
+
+class DonationTaxReceipt(models.Model):
+    _name = 'donation.tax.receipt'
+    _description = "Tax Receipt for Donations"
+    _order = 'id desc'
+    _rec_name = 'number'
+
+    number = fields.Char(string='Receipt Number')
+    date = fields.Date(
+        string='Date', required=True, default=fields.Date.context_today)
+    donation_date = fields.Date(string='Donation Date')
+    amount = fields.Monetary(
+        string='Amount', digits=dp.get_precision('Account'),
+        currency_field='currency_id')
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency', required=True, ondelete='restrict')
+    partner_id = fields.Many2one(
+        'res.partner', string='Donor', required=True, ondelete='restrict',
+        domain=[('parent_id', '=', False)])
+    company_id = fields.Many2one(
+        'res.company', string='Company', required=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+            'donation.tax.receipt'))
+    print_date = fields.Date(string='Print Date')
+    type = fields.Selection([
+        ('each', 'One-Time Tax Receipt'),
+        ('annual', 'Annual Tax Receipt'),
+        ], string='Type', required=True)
+
+    # Maybe we can drop that code with the new seq management on v9
+    @api.model
+    def create(self, vals=None):
+        if vals is None:
+            vals = {}
+        date = vals.get('donation_date')
+        vals['number'] = self.env['ir.sequence'].with_context(
+            date=date).next_by_code('donation.tax.receipt')
+        return super(DonationTaxReceipt, self).create(vals)
