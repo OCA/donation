cfe4c6eb2952bc8d6544fdcd4e6906cbb8a1434d
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-12 20:44:28 +0200

    Port all modules (except donation_thanks) to v10

--- donation_bank_statement/models/bank_statement.py
+++ donation_bank_statement/models/bank_statement.py
@@ -3,9 +3,9 @@
 # © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
-from openerp.tools import float_compare
-from openerp.exceptions import UserError
+from odoo import models, fields, api, _
+from odoo.tools import float_compare
+from odoo.exceptions import UserError
 
 
 class AccountBankStatement(models.Model):
