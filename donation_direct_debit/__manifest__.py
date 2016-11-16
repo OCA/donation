2fbd19f511dfcd7c6419baba7a6123dffbf4b30e
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-16 14:55:28 +0100

    Pylint fixes

--- donation_direct_debit/__manifest__.py
+++ donation_direct_debit/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Donation Direct Debit',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Auto-generate direct debit order on donation validation',
