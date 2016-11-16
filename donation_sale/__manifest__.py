e58d22b720e6973d7b391f319a26b4713ba954ee
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-16 14:55:28 +0100

    Pylint fixes

--- donation_sale/__manifest__.py
+++ donation_sale/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Donation Sale',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Manage donations in sale orders',
