f3fab51a22ece8ae8cd35fde3bf2ca59a2feff23
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-16 14:55:28 +0100

    Pylint fixes

--- donation_bank_statement/__manifest__.py
+++ donation_bank_statement/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Donation Bank Statement',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Creates donation from unreconciled bank statement lines',
