e7328c6e6199af4b93a57bf8f2e46ae402540c07
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-06 14:22:09 +0100

    PEP8 fixes

--- donation_tax_receipt/wizard/tax_receipt_option_switch.py
+++ donation_tax_receipt/wizard/tax_receipt_option_switch.py
@@ -1,28 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Donation Tax Receipt module for Odoo
-#    Copyright (C) 2014-2017 Barroux Abbey (www.barroux.org)
-#    Copyright (C) 2014-2017 Akretion France (www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2017 Barroux Abbey (www.barroux.org)
+# © 2017 Akretion France (www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
 
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning as UserError
+
+from openerp import models, fields, api
 
 
 class DonationTaxReceiptOptionSwitch(models.TransientModel):
