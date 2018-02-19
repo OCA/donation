435a8a83984fa508594c73d908168bc3dce6cbf0
Alexis de Lattre <alexis.delattre@akretion.com>
2018-02-19 15:39:13 +0100

    donation_bank_statement: No more button *Create Donations* on bank statements ; it is now automatic when you validate the bank statement.

--- donation_bank_statement/models/bank_statement.py
+++ donation_bank_statement/models/bank_statement.py
@@ -11,6 +11,20 @@ from odoo.exceptions import UserError
 class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement'
 
+    donation_ids = fields.One2many(
+        'donation.donation', 'bank_statement_id', string='Donations',
+        readonly=True)
+    donation_count = fields.Integer(
+        compute='_compute_donation_count', readonly=True,
+        string="# of Donations")
+
+    def _compute_donation_count(self):
+        for stmt in self:
+            try:
+                stmt.donation_count = len(stmt.donation_ids)
+            except:
+                stmt.donation_count = 0
+
     @api.model
     def _find_donation_product(self, stline, company):
         '''This method is designed to be inherited'''
@@ -45,42 +59,39 @@ class AccountBankStatement(models.Model):
             }
         return vals
 
-    @api.multi
     def create_donations(self):
         self.ensure_one()
         ddo = self.env['donation.donation']
-        precision = self.env['decimal.precision'].precision_get('Account')
-        assert self.state == 'confirm',\
-            'Statement must be in confirm state'
+        prec = self.currency_id.rounding
         if not self.company_id.donation_credit_transfer_journal_id:
-            raise UserError(
-                _("You must configure the Journal for Donations via "
-                    "Credit Transfer for the company %s")
+            raise UserError(_(
+                "You must configure the Journal for Donations via "
+                "Credit Transfer for the company %s")
                 % self.company_id.name)
         journal = self.company_id.donation_credit_transfer_journal_id
         if not journal.default_debit_account_id:
-            raise UserError(
-                _("Missing Default Debit Account on the Journal '%s'.")
+            raise UserError(_(
+                "Missing Default Debit Account on the Journal '%s'.")
                 % journal.name)
         donation_ids = []
         transit_account = journal.default_debit_account_id
         for stline in self.line_ids:
             if (
                     float_compare(
-                        stline.amount, 0, precision_digits=precision) == 1 and
+                        stline.amount, 0, precision_rounding=prec) == 1 and
                     not stline.donation_ids):
                 for amline in stline.journal_entry_ids:
                     for mline in amline.line_ids:
                         if (
                                 float_compare(
                                     mline.credit, 0,
-                                    precision_digits=precision) == 1 and
+                                    precision_rounding=prec) == 1 and
                                 mline.account_id == transit_account and
                                 not mline.reconciled):
                             if not stline.partner_id:
-                                raise UserError(
-                                    _("Missing partner on bank statement line "
-                                        "'%s' dated %s with amount %s.")
+                                raise UserError(_(
+                                    "Missing partner on bank statement line "
+                                    "'%s' dated %s with amount %s.")
                                     % (stline.name, stline.date,
                                         stline.amount))
                             vals = self._prepare_donation_vals(stline, mline)
@@ -89,17 +100,12 @@ class AccountBankStatement(models.Model):
                             donation.line_ids.product_id_change()
                             donation_ids.append(donation.id)
 
-        if not donation_ids:
-            raise UserError(
-                _('No new donation to generate for this bank statement.'))
-        action = self.env['ir.actions.act_window'].for_xml_id(
-            'donation', 'donation_action')
-        action.update({
-            'view_mode': 'tree,form,graph',
-            'domain': [('id', 'in', donation_ids)],
-            'target': 'current',
-            })
-        return action
+    @api.multi
+    def button_confirm_bank(self):
+        statements = self.filtered(lambda st: st.state == 'open')
+        for stmt in statements:
+            stmt.create_donations()
+        return super(AccountBankStatement, self).button_confirm_bank()
 
 
 class AccountBankStatementLine(models.Model):
