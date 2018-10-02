590496b40dec19fac3085d1b8f9a954e718bd0de
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-02 19:37:55 +0200

    Fix tests on runbot
    donation_bank_statement: add domain type=bank on donation_credit_transfer_journal_id
    donation: Small code improvement on income account selection from product
    PEP8 fix

--- donation_recurring/tests/test_recurring_donations.py
+++ donation_recurring/tests/test_recurring_donations.py
@@ -1,19 +1,94 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
+import time
 
 
 class TestDonationRecurring(TransactionCase):
+
+    at_install = False
+    post_install = True
+
+    def setUp(self):
+        super(TestDonationRecurring, self).setUp()
+        self.test_company = self.env['res.company'].create({
+            'name': 'TestCharity',
+            })
+        self.env.user.write({
+            'company_id': self.test_company.id,
+            'company_ids': [(4, self.test_company.id)],
+            })
+
+        generic_coa = self.env.ref(
+            'l10n_generic_coa.configurable_chart_template')
+        generic_coa.try_loading_for_current_company()
+        # Warning: loading coa chart of accounts writes
+        # USD as currency of company (even if company was created with
+        # EUR as currency. So let's work on USD for the tests
+
+        self.currency = self.test_company.currency_id
+        self.bank_journal = self.env['account.journal'].search([
+            ('company_id', '=', self.test_company.id),
+            ('type', '=', 'bank')], limit=1)
+        self.product = self.env.ref(
+            'donation_base.product_product_donation')
+        self.inkind_product = self.env.ref(
+            'donation_base.product_product_inkind_donation')
+        self.ddo = self.env['donation.donation']
+        self.don_rec1 = self.ddo.create({
+            'company_id': self.test_company.id,
+            'currency_id': self.currency.id,
+            'check_total': 30,
+            'partner_id': self.env.ref('donation_recurring.donor_rec1').id,
+            'donation_date': time.strftime('%Y-01-01'),
+            'journal_id': self.bank_journal.id,
+            'tax_receipt_option': 'annual',
+            'recurring_template': 'active',
+            'line_ids': [(0, 0, {
+                'product_id': self.product.id,
+                'quantity': 1,
+                'unit_price': 30,
+                })],
+            })
+        self.don_rec2 = self.ddo.create({
+            'company_id': self.test_company.id,
+            'currency_id': self.currency.id,
+            'check_total': 25,
+            'partner_id': self.env.ref('donation_recurring.donor_rec2').id,
+            'donation_date': time.strftime('%Y-01-01'),
+            'journal_id': self.bank_journal.id,
+            'tax_receipt_option': 'annual',
+            'recurring_template': 'active',
+            'line_ids': [(0, 0, {
+                'product_id': self.product.id,
+                'quantity': 1,
+                'unit_price': 25,
+                })],
+            })
+        self.don_rec3 = self.ddo.create({
+            'company_id': self.test_company.id,
+            'currency_id': self.currency.id,
+            'check_total': 35,
+            'partner_id': self.env.ref('donation_recurring.donor_rec3').id,
+            'donation_date': time.strftime('%Y-01-01'),
+            'journal_id': self.bank_journal.id,
+            'tax_receipt_option': 'annual',
+            'recurring_template': 'suspended',
+            'line_ids': [(0, 0, {
+                'product_id': self.product.id,
+                'quantity': 1,
+                'unit_price': 35,
+                })],
+            })
 
     def test_donation_recurring(self):
         wizard = self.env['donation.recurring.generate'].create(
             {'payment_ref': 'Don Abbaye Sainte Madeleine'})
         action = wizard.generate()
-        active_don_recs = [
-            self.env.ref('donation_recurring.donation_rec1'),
-            self.env.ref('donation_recurring.donation_rec2')]
+        active_don_recs = self.don_rec1 + self.don_rec2
         for don_rec in active_don_recs:
             self.assertTrue(don_rec.recurring_donation_ids)
             don = don_rec.recurring_donation_ids[0]
@@ -23,8 +98,7 @@ class TestDonationRecurring(TransactionCase):
                 don.payment_ref,
                 'Don Abbaye Sainte Madeleine')
             self.assertEquals(don.campaign_id, don_rec.campaign_id)
-        don_rec3 = self.env.ref('donation_recurring.donation_rec3')
-        self.assertFalse(don_rec3.recurring_donation_ids)
+        self.assertFalse(self.don_rec3.recurring_donation_ids)
         active_ids = action['domain'][0][2]
         wizard_val = self.env['donation.validate'].with_context(
             active_ids=active_ids, active_model='donation.donation').\
