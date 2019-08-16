d3df0dd2c5a796772e4c3964537edea946df486e
tbaden <thore.baden@gmx.net>
2019-08-16 10:34:13 +0200

    [11.0][NEW][WIP] donation_inkind module

---
+++ donation_inkind/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Donation in kind',
+    'version': '11.0.1.0.0',
+    'summary': 'Module to manage physical inkind donations',
+    'category': 'Some Category',
+    'license': 'AGPL-3',
+    'author': 'Benjamin Brich, Thore Baden, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/donation',
+    'contributors': [
+        'Benjamin Brich',
+        'Thore Baden'
+    ],
+    'depends': [
+        'donation',
+        'stock',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ donation_inkind/models/product.py
@@ -0,0 +1,16 @@
+# © 2019 Benjamin Brich <@>
+# © 2019 Thore Baden <thorebaden@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.constrains('donation', 'type')
+    def donation_check(self):
+        for product in self:
+            if product.donation and product.type != 'product':
+                super().donation_check()
