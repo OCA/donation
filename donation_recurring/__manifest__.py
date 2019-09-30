db65d110dec0f66312a95de746e80f2c26081ded
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2019-10-01 00:38:47 +0530

    [MIG] Migration: donation_recurring  v11 to v12

--- donation_recurring/__manifest__.py
+++ donation_recurring/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Barroux Abbey (http://www.barroux.org)
 # © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Donation Recurring',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Manage recurring donations',
