42575d737186de705f8b62817dce941472e9f239
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2019-10-01 00:02:06 +0530

    [MIG] Migration: donation_base v11 to v12

--- donation_base/__manifest__.py
+++ donation_base/__manifest__.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Barroux Abbey (http://www.barroux.org)
 # © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Donation Base',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Base module for donations',
     'author': 'Barroux Abbey, Akretion, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/donation',
     'depends': [
-        'account_invoicing',
+        'account',
     ],
     'data': [
         'security/ir.model.access.csv',
