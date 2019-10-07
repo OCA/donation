0a1c08ddcde7c433ad0dd47a5b64dff76dca1db0
aenaya <amjad.enaya@initos.com>
2019-10-07 16:54:50 +0200

    solve ci issues

--- donation/models/res_config_settings.py
+++ donation/models/res_config_settings.py
@@ -2,7 +2,8 @@
 # 2019 initOS GmbH (Amjad Enaya <amjad.enaya@initos.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
+from odoo import models, fields
+
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
@@ -11,5 +12,7 @@ class ResConfigSettings(models.TransientModel):
     module_donation_bank_statement = fields.Boolean('Donation Bank Statement')
     module_donation_direct_debit = fields.Boolean('Donation Direct Debit')
     module_donation_recurring = fields.Boolean('Donation Recurring')
+
+
 
 
