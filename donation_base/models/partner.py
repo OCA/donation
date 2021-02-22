e3922c610bd4a02dba9a61f2d7f13b67812199c1
Alba Riera <alba.riera@creublanca.es>
2021-02-22 12:52:31 +0100

    [IMP] donation_base: black, isort, prettier

--- donation_base/models/partner.py
+++ donation_base/models/partner.py
@@ -2,39 +2,37 @@
 # © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    tax_receipt_option = fields.Selection([
-        ('none', 'None'),
-        ('each', 'For Each Donation'),
-        ('annual', 'Annual Tax Receipt')],
-        string='Tax Receipt Option',
-        default='each',
-        track_visibility='onchange'
+    tax_receipt_option = fields.Selection(
+        [
+            ("none", "None"),
+            ("each", "For Each Donation"),
+            ("annual", "Annual Tax Receipt"),
+        ],
+        string="Tax Receipt Option",
+        default="each",
+        track_visibility="onchange",
     )
     tax_receipt_ids = fields.One2many(
-        'donation.tax.receipt',
-        'partner_id',
-        string='Tax Receipts'
+        "donation.tax.receipt", "partner_id", string="Tax Receipts"
     )
     tax_receipt_count = fields.Integer(
-        compute='_compute_tax_receipt_count',
-        string="# of Tax Receipts",
-        readonly=True
+        compute="_compute_tax_receipt_count", string="# of Tax Receipts", readonly=True
     )
-    donor = fields.Boolean(string='Donor')
+    donor = fields.Boolean(string="Donor")
 
     @api.model
     def _commercial_fields(self):
         res = super(ResPartner, self)._commercial_fields()
-        res.append('tax_receipt_option')
+        res.append("tax_receipt_option")
         return res
 
-    @api.depends('tax_receipt_ids')
+    @api.depends("tax_receipt_ids")
     def _compute_tax_receipt_count(self):
         for partner in self:
             partner.tax_receipt_count = len(partner.tax_receipt_ids.ids)
--- donation_base/readme/CONFIGURE.rst
+++ donation_base/readme/CONFIGURE.rst
@@ -2,4 +2,3 @@ To configure this module, you need to:
 
  * create donation products
  * set the *Tax Receipt Option* on partners
-
