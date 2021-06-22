a3dd6c21c325d17f5527057c05416347e67e0aa6
Alba Riera <alba.riera@creublanca.es>
2021-06-22 15:45:19 +0200

    [IMP] donation: black, isort, prettier

--- donation/models/partner.py
+++ donation/models/partner.py
@@ -2,25 +2,20 @@
 # Copyright 2014-2016 Akretion France
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    @api.depends('donation_ids.partner_id')
+    @api.depends("donation_ids.partner_id")
     def _compute_donation_count(self):
         for partner in self:
             partner.donation_count = len(partner.donation_ids.ids)
 
     donation_ids = fields.One2many(
-        'donation.donation',
-        'partner_id',
-        string='Donations',
-        readonly=True
+        "donation.donation", "partner_id", string="Donations", readonly=True
     )
     donation_count = fields.Integer(
-        compute='_compute_donation_count',
-        string="# of Donations",
-        readonly=True
+        compute="_compute_donation_count", string="# of Donations", readonly=True
     )
