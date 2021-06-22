3a056be3f221693821934d845e73acd8416f1f2e
Alba Riera <alba.riera@creublanca.es>
2021-06-22 15:45:19 +0200

    [IMP] donation: black, isort, prettier

--- donation/models/donation_campaign.py
+++ donation/models/donation_campaign.py
@@ -2,31 +2,25 @@
 # Copyright 2014-2016 Akretion France
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class DonationCampaign(models.Model):
-    _name = 'donation.campaign'
-    _description = 'Code attributed for a Donation Campaign'
-    _order = 'code'
+    _name = "donation.campaign"
+    _description = "Code attributed for a Donation Campaign"
+    _order = "code"
 
-    @api.depends('code', 'name')
+    @api.depends("code", "name")
     def name_get(self):
         res = []
         for camp in self:
             name = camp.name
             if camp.code:
-                name = u'[%s] %s' % (camp.code, name)
+                name = u"[{}] {}".format(camp.code, name)
             res.append((camp.id, name))
         return res
 
-    code = fields.Char('Code')
-    name = fields.Char(
-        'Name',
-        required=True
-    )
-    start_date = fields.Date(
-        'Start Date',
-        default=fields.Date.context_today
-    )
-    note = fields.Text('Notes', oldname='nota')
+    code = fields.Char("Code")
+    name = fields.Char("Name", required=True)
+    start_date = fields.Date("Start Date", default=fields.Date.context_today)
+    note = fields.Text("Notes", oldname="nota")
--- donation/post_install.py
+++ donation/post_install.py
@@ -1,14 +1,12 @@
 # Copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def update_account_journal(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        journals = env['account.journal'].search(
-            [('type', 'in', ('bank', 'cash'))]
-        )
-        journals.write({'allow_donation': True})
+        journals = env["account.journal"].search([("type", "in", ("bank", "cash"))])
+        journals.write({"allow_donation": True})
     return
