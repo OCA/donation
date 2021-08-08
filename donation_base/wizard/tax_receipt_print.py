ebf7d0bb1060929cb5d2043acecf84ade42d35a1
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-09 01:23:24 +0200

    [MIG] donation_base from v13 to v14

--- donation_base/wizard/tax_receipt_print.py
+++ donation_base/wizard/tax_receipt_print.py
@@ -1,5 +1,6 @@
-# © 2014-2016 Barroux Abbey (http://www.barroux.org)
-# © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2014-2021 Barroux Abbey (http://www.barroux.org)
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
@@ -18,14 +19,16 @@ class DonationTaxReceiptPrint(models.TransientModel):
         "donation.tax.receipt",
         column1="print_wizard_id",
         column2="receipt_id",
-        string="Receipts To Print",
-        default=_get_receipts,
+        string="Receipts to Print",
+        default=lambda self: self._get_receipts(),
     )
 
     def print_receipts(self):
         self.ensure_one()
         if not self.receipt_ids:
             raise UserError(_("There are no tax receipts to print."))
+        today = fields.Date.context_today(self)
+        self.receipt_ids.write({"print_date": today})
         return self.env.ref("donation_base.report_donation_tax_receipt").report_action(
             self.receipt_ids
         )
