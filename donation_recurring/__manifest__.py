b08cf6d2d156ef8b8be97182751bbaa3a085c52a
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-12 20:16:12 +0200

    donation_recurring: black, isort and other automatic reformatting

--- donation_recurring/__manifest__.py
+++ donation_recurring/__manifest__.py
@@ -4,23 +4,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Donation Recurring',
-    'version': '14.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'summary': 'Manage recurring donations',
-    'author': 'Barroux Abbey, Akretion, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/donation',
-    'depends': [
-        'donation',
+    "name": "Donation Recurring",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Manage recurring donations",
+    "author": "Barroux Abbey, Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/donation",
+    "depends": [
+        "donation",
     ],
-    'data': [
-        'views/donation.xml',
-        'wizard/donation_recurring_generate_view.xml',
-        'security/ir.model.access.csv',
+    "data": [
+        "views/donation.xml",
+        "wizard/donation_recurring_generate_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'demo': [
-        'demo/donation_recurring_demo.xml'
-    ],
-    'installable': True,
+    "demo": ["demo/donation_recurring_demo.xml"],
+    "installable": True,
 }
--- donation_recurring/models/donation.py
+++ donation_recurring/models/donation.py
@@ -3,82 +3,85 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class DonationDonation(models.Model):
-    _inherit = 'donation.donation'
+    _inherit = "donation.donation"
 
-    recurring_template = fields.Selection([
-        ('active', 'Active'),
-        ('suspended', 'Suspended')],
-        string='Recurring Template',
+    recurring_template = fields.Selection(
+        [("active", "Active"), ("suspended", "Suspended")],
+        string="Recurring Template",
         copy=False,
         index=True,
         tracking=True,
     )
     source_recurring_id = fields.Many2one(
-        'donation.donation',
-        string='Source Recurring Template',
-        states={'done': [('readonly', True)]}
+        "donation.donation",
+        string="Source Recurring Template",
+        states={"done": [("readonly", True)]},
     )
     recurring_donation_ids = fields.One2many(
-        'donation.donation',
-        'source_recurring_id',
-        string='Past Recurring Donations',
+        "donation.donation",
+        "source_recurring_id",
+        string="Past Recurring Donations",
         readonly=True,
-        copy=False
+        copy=False,
     )
 
     @api.constrains(
-        'recurring_template', 'source_recurring_id', 'state',
-        'tax_receipt_option')
+        "recurring_template", "source_recurring_id", "state", "tax_receipt_option"
+    )
     def _check_recurring_donation(self):
         for donation in self:
-            if donation.recurring_template and donation.state != 'draft':
-                raise ValidationError(_(
-                    "The recurring donation template %s must stay in "
-                    "draft state.") % donation.number)
+            if donation.recurring_template and donation.state != "draft":
+                raise ValidationError(
+                    _("The recurring donation template %s must stay in " "draft state.")
+                    % donation.number
+                )
             if donation.source_recurring_id and donation.recurring_template:
-                raise ValidationError(_(
-                    "The recurring donation template %s cannot have "
-                    "a Source Recurring Template")
-                    % donation.number)
-            if (
-                    donation.recurring_template and
-                    donation.tax_receipt_option == 'each'):
-                raise ValidationError(_(
-                    "The recurring donation %s cannot have a tax "
-                    "receipt option 'Each'.")
-                    % donation.number)
+                raise ValidationError(
+                    _(
+                        "The recurring donation template %s cannot have "
+                        "a Source Recurring Template"
+                    )
+                    % donation.number
+                )
+            if donation.recurring_template and donation.tax_receipt_option == "each":
+                raise ValidationError(
+                    _(
+                        "The recurring donation %s cannot have a tax "
+                        "receipt option 'Each'."
+                    )
+                    % donation.number
+                )
 
-    @api.depends('state', 'partner_id', 'move_id', 'recurring_template')
+    @api.depends("state", "partner_id", "move_id", "recurring_template")
     def name_get(self):
         res = []
         for donation in self:
-            if donation.recurring_template == 'active':
-                name = _('Recurring Donation %s') % (
-                    donation.number)
-            elif donation.recurring_template == 'suspended':
-                name = _('Suspended Recurring Donation %s') % (
-                    donation.number)
+            if donation.recurring_template == "active":
+                name = _("Recurring Donation %s") % (donation.number)
+            elif donation.recurring_template == "suspended":
+                name = _("Suspended Recurring Donation %s") % (donation.number)
             else:
                 name = super(DonationDonation, donation).name_get()[0][1]
             res.append((donation.id, name))
         return res
 
-    @api.onchange('recurring_template')
+    @api.onchange("recurring_template")
     def recurring_template_change(self):
-        res = {'warning': {}}
-        if self.recurring_template and self.tax_receipt_option == 'each':
-            self.tax_receipt_option = 'annual'
-            res['warning']['title'] = _('Update of Tax Receipt Option')
-            res['warning']['message'] = _(
+        res = {"warning": {}}
+        if self.recurring_template and self.tax_receipt_option == "each":
+            self.tax_receipt_option = "annual"
+            res["warning"]["title"] = _("Update of Tax Receipt Option")
+            res["warning"]["message"] = _(
                 "As it is a recurring donation, "
                 "the Tax Receipt Option has been changed from Each to "
                 "Annual. You may want to change it also on the Donor "
-                "form.")
+                "form."
+            )
         if not self.recurring_template and self.commercial_partner_id:
             if self.commercial_partner_id.tax_receipt_option != self.tax_receipt_option:
                 self.tax_receipt_option = self.commercial_partner_id.tax_receipt_option
@@ -86,10 +89,10 @@ class DonationDonation(models.Model):
 
     def active2suspended(self):
         self.ensure_one()
-        assert self.recurring_template == 'active'
-        self.write({'recurring_template': 'suspended'})
+        assert self.recurring_template == "active"
+        self.write({"recurring_template": "suspended"})
 
     def suspended2active(self):
         self.ensure_one()
-        assert self.recurring_template == 'suspended'
-        self.write({'recurring_template': 'active'})
+        assert self.recurring_template == "suspended"
+        self.write({"recurring_template": "active"})
