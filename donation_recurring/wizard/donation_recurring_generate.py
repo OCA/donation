03a9125ddd8d7eec0d5e403dd375f91613c25228
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-12 20:10:22 +0200

    [MIG] donation_recurring from v12 to v14
    Add buttons to suspend/re-activate
    Add a red banner on suspended recurring donations

--- donation_recurring/wizard/donation_recurring_generate.py
+++ donation_recurring/wizard/donation_recurring_generate.py
@@ -1,9 +1,11 @@
-# © 2014-2016 Barroux Abbey (http://www.barroux.org)
-# © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2014-2021 Barroux Abbey (http://www.barroux.org)
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+from odoo.tools.misc import format_date
 
 
 class DonationRecurringGenerate(models.TransientModel):
@@ -16,6 +18,9 @@ class DonationRecurringGenerate(models.TransientModel):
         default=fields.Date.context_today
     )
     payment_ref = fields.Char(string='Payment Reference')
+    company_id = fields.Many2one(
+        'res.company', required=True, string='Company',
+        default=lambda self: self.env.company)
 
     @api.model
     def _prepare_donation_default(self, donation):
@@ -31,25 +36,23 @@ class DonationRecurringGenerate(models.TransientModel):
         doo = self.env['donation.donation']
         donations = doo.search([
             ('recurring_template', '=', 'active'),
-            ('company_id', '=', self.env.user.company_id.id)])
+            ('company_id', '=', self.company_id.id)])
         new_donation_ids = []
         existing_recur_donations = doo.search([
             ('donation_date', '=', self.date),
             ('source_recurring_id', '!=', False),
-            ('company_id', '=', self.env.user.company_id.id)])
+            ('company_id', '=', self.company_id.id)])
         if existing_recur_donations:
             raise UserError(
                 _('Recurring donations have already been generated for %s.')
-                % self.date)
+                % format_date(self.env, self.date))
         for donation in donations:
             default = self._prepare_donation_default(donation)
             new_donation = donation.copy(default=default)
             new_donation_ids.append(new_donation.id)
-        action = self.env.ref('donation.donation_action').read()[0]
+        action = self.env.ref('donation.donation_action').sudo().read([])[0]
         action.update({
-            'view_mode': 'tree,form,graph',
             'domain': [('id', 'in', new_donation_ids)],
-            'target': 'current',
             'limit': 500,
         })
         return action
