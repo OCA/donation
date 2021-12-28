2e7792894f6de221a4e10599a59a21a43815c324
Alexis de Lattre <alexis.delattre@akretion.com>
2021-12-29 00:39:11 +0100

    product_analytic_donation: black, isort, etc.

--- product_analytic_donation/__manifest__.py
+++ product_analytic_donation/__manifest__.py
@@ -3,15 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Product Analytic Donation',
-    'version': '14.0.1.0.0',
-    'category': 'Donation',
-    'license': 'AGPL-3',
-    'summary': 'Glue module between donation and product_analytic',
-    'author': 'Akretion, Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/donation',
-    'depends': ['donation', 'product_analytic'],
-    'auto_install': True,
-    'installable': True,
+    "name": "Product Analytic Donation",
+    "version": "14.0.1.0.0",
+    "category": "Donation",
+    "license": "AGPL-3",
+    "summary": "Glue module between donation and product_analytic",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/donation",
+    "depends": ["donation", "product_analytic"],
+    "auto_install": True,
+    "installable": True,
 }
--- product_analytic_donation/models/donation_line.py
+++ product_analytic_donation/models/donation_line.py
@@ -6,24 +6,25 @@ from odoo import api, models
 
 
 class DonationLine(models.Model):
-    _inherit = 'donation.line'
+    _inherit = "donation.line"
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         res = super(DonationLine, self).product_id_change()
         if self.product_id:
-            ana_accounts = self.product_id.product_tmpl_id.\
-                _get_product_analytic_accounts()
-            ana_account = ana_accounts['income']
+            ana_accounts = (
+                self.product_id.product_tmpl_id._get_product_analytic_accounts()
+            )
+            ana_account = ana_accounts["income"]
             if ana_account:
                 self.analytic_account_id = ana_account.id
         return res
 
     @api.model
     def create(self, vals):
-        if vals.get('product_id') and not vals.get('analytic_account_id'):
-            product = self.env['product.product'].browse(vals['product_id'])
+        if vals.get("product_id") and not vals.get("analytic_account_id"):
+            product = self.env["product.product"].browse(vals["product_id"])
             ana_accounts = product.product_tmpl_id._get_product_analytic_accounts()
-            ana_account = ana_accounts['income']
-            vals['analytic_account_id'] = ana_account.id or False
+            ana_account = ana_accounts["income"]
+            vals["analytic_account_id"] = ana_account.id or False
         return super().create(vals)
