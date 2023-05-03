21184a5f78b12bf32ffe10c4bf5dcabac4bf8cb9
Diego Paradeda <paradeda.diego@gmail.com>
2023-05-03 11:55:14 -0300

    [ADD] use try when setting order contribution

--- donation_pos/models/pos_order.py
+++ donation_pos/models/pos_order.py
@@ -17,9 +17,9 @@ class PosOrder(models.Model):
             order_id = order_ids.filtered(
                 lambda o_id: o_id.pos_reference == o.get("data").get("name")
             )
-            if not order_id:
+            try:
+                order_id.contribution = o.get("data").get("contribution")
+            except Exception:
                 continue
-            if o and "data" in o and "contribution" in o.get("data"):
-                order_id.contribution = o.get("data").get("contribution") 
 
         return res
