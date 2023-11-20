f3541fc6ef5585e103a33f484e2bce75a168c5ef
Alexis de Lattre <alexis.delattre@akretion.com>
2023-11-21 00:29:39 +0100

    [FIX] donation by direct debit
    For donation by debit order, we can't use twice the account "payment_debit_account_id", when
    validating the donation AND when validation the debit order. We have to
    use a different account. And, now that the OCA module
    account_payment_order uses account.payment, this account must be a
    receivable account.

---
+++ donation/models/account_journal.py
@@ -0,0 +1,58 @@
+# Copyright 2023 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    donation_debit_order_account_id = fields.Many2one(
+        "account.account",
+        check_company=True,
+        copy=False,
+        ondelete="restrict",
+        domain="[('reconcile', '=', True), ('deprecated', '=', False), "
+        "('company_id', '=', company_id), "
+        "('account_type', '=', 'asset_receivable'), "
+        "('id', 'not in', (default_account_id, suspense_account_id))]",
+        string="Donation by Debit Order Account",
+        help="Transfer account for donations by debit order. "
+        "Leave empty if you don't handle donations by debit order on this bank account."
+        "This account must be a receivable account, otherwise the debit order will not work.",
+    )
+
+    @api.constrains("donation_debit_order_account_id")
+    def _check_donation_accounts(self):
+        acc_type2label = dict(
+            self.env["account.account"].fields_get("account_type", "selection")[
+                "account_type"
+            ]["selection"]
+        )
+        for journal in self:
+            ddo_account = journal.donation_debit_order_account_id
+            if ddo_account:
+                if not ddo_account.reconcile:
+                    raise ValidationError(
+                        _(
+                            "The Donation by Debit Order Account of journal "
+                            "'%(journal)s' must be reconciliable, but the account "
+                            "'%(account)s' is not reconciliable.",
+                            journal=journal.display_name,
+                            account=ddo_account.display_name,
+                        )
+                    )
+                if ddo_account.account_type != "asset_receivable":
+                    raise ValidationError(
+                        _(
+                            "The Donation by Debit Order Account of journal "
+                            "'%(journal)s' must be a receivable account, "
+                            "but the account '%(account)s' is configured with "
+                            "account type '%(account_type)s'.",
+                            journal=journal.display_name,
+                            account=ddo_account.display_name,
+                            account_type=acc_type2label[ddo_account.account_type],
+                        )
+                    )
