2e230aa4aed5779466de77edce35af26f185d87f
brendaf <brenda.fernandez@fundesplai.org>
2024-04-23 15:12:33 +0200

    (98 > 88 characters)

--- donation_certificate/models/donation.py
+++ donation_certificate/models/donation.py
@@ -40,7 +40,9 @@ class Donation(models.Model):
             default_template_id=template and template.id or False,
             default_composition_mode="comment",
             mark_invoice_as_sent=True,
-            default_email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature",
+            default_email_layout_xmlid=(
+                "mail.mail_notification_layout_with_responsible_signature"
+            ),
             force_email=True,
             active_ids=self.tax_receipt_id.ids,
         )
